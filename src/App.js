62c66d300ded4e5afe5ae178bb21883726c33913
Ishaan Deep <ishaandeep5@gmail.com>
2022-05-04 01:00:08 +0530

    removed warning from the App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+    </div>
+  );
+}
+
+export default App;
